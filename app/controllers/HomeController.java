055c928be341dc01f308efddf7479e95158a1e9b
Timmyy <x001225270@gmail.com>
2016-11-18 21:31:19 +0000

    third commit

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,31 @@
+package controllers;
+import play.api.Environment;
+import play.data.*;
+import play.mvc.*;
+import play.db.ebean.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import views.html.*;
+import models.*;
+
+public class HomeController extends Controller {
+
+    public Result index(String name) {
+        return ok(index.render("Welcome to the Home page", name));
+    }
+
+    public Result about() {
+        return ok(about.render());
+    }
+
+    public Result products() {
+		List<Product> productsList = Product.findAll();
+		
+        return ok(products.render(productsList));
+    }
+
+}
+
+
